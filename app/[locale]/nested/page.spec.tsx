df421b82d9aa69069eb98a7486c0d50ee8730f58
Sayan De <sayandeten@gmail.com>
2024-03-28 13:38:43 +0530

    add

--- app/[locale]/nested/page.spec.tsx
+++ app/[locale]/nested/page.spec.tsx
@@ -1,20 +1,20 @@
-import {render, screen} from '@testing-library/react';
-import pick from 'lodash/pick';
-import {NextIntlClientProvider} from 'next-intl';
-import messages from '../../../../messages/en.json';
-import Nested, {generateMetadata} from './page';
+import { render, screen } from "@testing-library/react";
+import pick from "lodash/pick";
+import { NextIntlClientProvider } from "next-intl";
+import messages from "../../../messages/en.json";
+import Nested, { generateMetadata } from "./page";
 
-it('renders', () => {
+it("renders", () => {
   render(
-    <NextIntlClientProvider locale="en" messages={pick(messages, ['Nested'])}>
+    <NextIntlClientProvider locale="en" messages={pick(messages, ["Nested"])}>
       <Nested />
     </NextIntlClientProvider>
   );
-  screen.getByText('Nested');
+  screen.getByText("Nested");
 });
 
 it("can't generate metadata in a test", async () => {
   await expect(generateMetadata()).rejects.toThrow(
-    '`getTranslations` is not supported in Client Components.'
+    "`getTranslations` is not supported in Client Components."
   );
 });
